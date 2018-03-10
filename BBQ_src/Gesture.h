001e8a8d3f293eba8cd7a4313affc6880b62cf64
Hodge Pai <hodge@quan.com.tw>
2018-03-10 09:15:51 +0800

    file arrange

---
+++ BBQ_src/Gesture.h
@@ -0,0 +1,9 @@
+
+#define BuffGestureSize	100
+extern uint8_t buffGesture[BuffGestureSize];
+extern uint8_t tailGesture;
+extern uint8_t headGesture;
+
+extern void initGestureChannel(void);
+extern uint8_t getDegree(void);
+	
